4473514cbd048fa30843c2b16b592604dc120591
Parteim <48876969+Parteim@users.noreply.github.com>
2023-04-22 16:49:29 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.BOT.Groups_methods import Groups
 
 def run():
     bot = Groups(
-        'f5f26c4d4e17f59026a331585c2287b1a2613a154fce512c02324f73e432119b51cf37cb7b7ec02b3658a',
+        '',
     )
 
     print(bot.connect())
